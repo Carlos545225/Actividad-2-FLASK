388c52467482e3217435f96669765cb8ce3b5fe1
Carlos545225 <115045524+Carlos545225@users.noreply.github.com>
2024-11-29 00:10:17 -0500

    Delete 4-app_crud_juegos directory

--- 4-app_crud_juegos/bd.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-def odtener_conexion():
-    return pymysql.connect(host='localhost',
-                           user='root',
-                           password='',
-                           db='app_crud_juego') 
