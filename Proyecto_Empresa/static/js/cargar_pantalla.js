5e859c86c40c00cde6105915583cf953ca2051dd
Carlos545225 <115045524+Carlos545225@users.noreply.github.com>
2025-03-04 20:55:53 -0500

    Add files via upload

---
+++ Proyecto_Empresa/static/js/cargar_pantalla.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none'; 
+  }, 5000);
+});
