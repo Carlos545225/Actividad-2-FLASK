03467ee52b266e4cabbee04373daa866852b3a72
Carlos545225 <115045524+Carlos545225@users.noreply.github.com>
2025-03-21 19:55:16 -0500

    Delete Proyecto_Empresa directory

--- Proyecto_Empresa/static/js/cargar_pantalla.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    const loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.display = 'none'; 
-  }, 5000);
-});
